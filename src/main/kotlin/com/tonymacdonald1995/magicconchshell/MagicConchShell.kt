6b16540d1557b5a8d7c080c227097ce36a5dea2f
Tony MacDonald <tonymacdonald1995@gmail.com>
2023-02-16 21:24:49 -0600

    Added Tod, a chatbot

--- src/main/kotlin/com/tonymacdonald1995/magicconchshell/MagicConchShell.kt
+++ src/main/kotlin/com/tonymacdonald1995/magicconchshell/MagicConchShell.kt
@@ -12,15 +12,20 @@ import com.theokanning.openai.OpenAiService
 import com.theokanning.openai.completion.CompletionRequest
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.audio.AudioSendHandler
+import net.dv8tion.jda.api.entities.Icon
+import net.dv8tion.jda.api.entities.MessageHistory
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.FileUpload
 import java.io.BufferedReader
 import java.io.DataOutputStream
+import java.io.File
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
@@ -41,32 +46,22 @@ fun main(args: Array<String>) {
         log("Error: No OpenAI token")
         return
     }
-    val magicConchShell = MagicConchShell()
-    magicConchShell.openAiToken = openAiToken
-    magicConchShell.openAiService = OpenAiService(openAiToken)
-    val jda = JDABuilder.createDefault(botToken).addEventListeners(magicConchShell).build()
-    jda.selfUser.manager.setName("Magic Conch Shell").queue()
+    val magicConchShell = MagicConchShell(openAiToken)
+    val jda = JDABuilder.createDefault(botToken).addEventListeners(magicConchShell).enableIntents(GatewayIntent.MESSAGE_CONTENT).build()
+    jda.selfUser.manager.setName("Tod").queue()
 }
-class MagicConchShell : ListenerAdapter() {
+class MagicConchShell(private val openAiToken: String) : ListenerAdapter() {
 
-    private val playerManager = DefaultAudioPlayerManager()
-    private val audioPlayers = mutableMapOf<Long, AudioPlayer>()
+    private val openAiService: OpenAiService = OpenAiService(openAiToken)
 
-    var openAiToken: String? = null
-    var openAiService: OpenAiService? = null
-
-    init {
-        AudioSourceManagers.registerRemoteSources(playerManager)
-    }
+    private var messageHistory : MutableList<String> = mutableListOf()
 
     override fun onGuildReady(event: GuildReadyEvent) {
         log("Connected to " + event.guild.name)
 
-        event.guild.selfMember.modifyNickname("Magic Conch Shell").queue()
-        event.guild.upsertCommand("image", "Generates an image based on the prompt given")
-            .addOption(OptionType.STRING, "prompt", "Prompt for image generation", true)
-            .queue()
-        event.guild.upsertCommand("join", "This is a test").queue()
+        event.jda.selfUser.manager.setAvatar(Pfp.pfp)
+        event.guild.selfMember.modifyNickname("Tod").queue()
+        event.guild.upsertCommand("reset-tod", "Reset Tod's memory").setDefaultPermissions(DefaultMemberPermissions.DISABLED).queue()
     }
 
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
@@ -74,125 +69,47 @@ class MagicConchShell : ListenerAdapter() {
             return
 
         when (event.name) {
-            "image" -> genImage(event)
-            "join" -> joinVoice(event)
+            "reset-tod" -> resetTod(event)
             else -> event.reply("Error: Unknown command").setEphemeral(true).queue()
         }
     }
 
-    private fun joinVoice(event: SlashCommandInteractionEvent) {
-        val member = event.member ?: return
-
-        if (member.voiceState?.channel == null)
-            event.reply("You aren't in a voice channel.")
-        event.reply("You have selected Microsoft Sam as the computer's default voice.")
-
-        val voiceChannel = member.voiceState?.channel?.asVoiceChannel() ?: return
-        playMicrosoftSam(voiceChannel)
-
-    }
-
-    private fun playMicrosoftSam(voiceChannel: VoiceChannel) {
-        val guild = voiceChannel.guild
-        val audioManager = guild.audioManager
-
-        if (!audioManager.isConnected)
-            audioManager.openAudioConnection(voiceChannel)
-
-        val player = playerManager.createPlayer()
-        audioPlayers[guild.idLong] = player
-        audioManager.sendingHandler = AudioHandler(player)
-
-        val trackUrl = "https://www.youtube.com/watch?v=P5DXddc5hOw"
-        playerManager.loadItem(trackUrl, object : AudioLoadResultHandler {
-            override fun trackLoaded(track: AudioTrack?) {
-                player.playTrack(track)
-            }
-
-            override fun playlistLoaded(playlist: AudioPlaylist?) {}
-
-            override fun noMatches() {}
-
-            override fun loadFailed(exception: FriendlyException?) {}
-
-        })
-        Thread {
-            while (player.playingTrack == null) {}
-            while (player.playingTrack != null) {}
-            audioManager.closeAudioConnection()
-        }.start()
-    }
-
-    private fun genImage(event: SlashCommandInteractionEvent) {
-        event.deferReply(true).queue()
-        val hook = event.hook
-        hook.setEphemeral(true)
-        val member = event.user.asMention
-        val channel = event.channel
-        val prompt = event.getOption("prompt")?.asString ?: return
-
-        val url = URL("https://api.openai.com/v1/images/generations")
-        val postData = "{" +
-                "\"prompt\": \"$prompt\"," +
-                "\"n\": 1," +
-                "\"size\": \"1024x1024\"" +
-                "}"
-
-        val conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "POST"
-        conn.doOutput = true
-        conn.setRequestProperty("Content-Type", "application/json")
-        conn.setRequestProperty("Authorization", "Bearer $openAiToken")
-        conn.useCaches = false
-
-        DataOutputStream(conn.outputStream).use { it.writeBytes(postData) }
-        var output = ""
-        val pattern = Pattern.compile("\"url\": \"([^\"]*)\"")
-        BufferedReader(InputStreamReader(conn.inputStream)).use { br ->
-            var line: String?
-            while (br.readLine().also { line = it } != null) {
-                val matcher = pattern.matcher(line.toString())
-                if (matcher.find())
-                    output = matcher.group(1)
-            }
+    private fun resetTod(event: SlashCommandInteractionEvent) {
+        if (event.channel.id != "1075951768564928593") {
+            event.reply("This command can only be used in " + event.guild?.getTextChannelById("1075951768564928593")?.asMention).setEphemeral(true).queue()
+            return
         }
-        hook.sendMessage("Done").queue()
-        channel.sendMessage("$member requested this image using the prompt: \"$prompt\"")
-            .addFiles(FileUpload.fromData(URL(output).openStream(), "image.png")).queue()
+
+        messageHistory = mutableListOf()
+        event.reply("Tod's memory has been reset.").queue()
     }
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
-        if (event.message.mentions.isMentioned(event.guild.selfMember)) {
+        if (event.message.channel.id == "1075951768564928593" && event.message.member != event.guild.selfMember) {
             event.message.channel.asTextChannel().sendTyping().queue()
+            messageHistory.add("[${event.message.member?.nickname}] ${event.message.contentDisplay}")
+            log("[messageHistory] [${event.message.member?.nickname}] ${event.message.contentDisplay}")
+            var prompt = "You are an anthropomorphic fox named Tod, participating is a group chat. You like to use rude an abrasive language and thrive in an environment of like-minded adults. You have no problem with cursing or foul language and use it regularly. Here is the chat history:\n"
+            for (message in messageHistory)
+                prompt += "$message\n"
+            prompt += "[Tod] "
             val completionRequest = CompletionRequest.builder()
-                .prompt(event.message.contentDisplay.removePrefix("@Magic Conch Shell "))
+                .prompt(prompt)
                 .model("text-davinci-003")
                 .echo(false)
-                .maxTokens(1000)
+                .maxTokens(2048)
+                .stop(mutableListOf("\n"))
                 .build()
             openAiService!!.createCompletion(completionRequest).choices.forEach {
+                if (it.text.isEmpty())
+                    onMessageReceived(event)
                 event.message.channel.asTextChannel().sendMessage(it.text).queue()
+                messageHistory.add("[Tod] ${it.text}")
+                log("[messageHistory] [Tod] ${it.text}")
             }
         }
     }
 
-}
-
-class AudioHandler(private val player: AudioPlayer) : AudioSendHandler {
-    private var lastFrame: AudioFrame? = null
-
-    override fun canProvide(): Boolean {
-        lastFrame = player.provide()
-        return lastFrame != null
-    }
-
-    override fun provide20MsAudio(): ByteBuffer? {
-        return ByteBuffer.wrap(lastFrame?.data ?: ByteArray(0))
-    }
-
-    override fun isOpus(): Boolean {
-        return true
-    }
 }
 
 fun log(message : String) {
